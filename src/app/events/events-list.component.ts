4294057c501ebdb91462753f57205981512ce9af
saslamp <sadiqasg@gmail.com>
2019-08-27 14:00:49 +0100

    initial commit for ng-fundamentals

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { EventService } from './shared/event.service';
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr />
+        <div class="row">
+            <div class="col-md-5" *ngFor="let event of events">
+                <event-thumbnail [event]="event"></event-thumbnail>
+            </div>
+        </div>
+    </div>
+    `
+})
+
+export class EventsListComponent {
+  events: any[]
+  
+  constructor(private eventService: EventService) {
+    this.events = this.eventService.getEvents()
+  }
+}
